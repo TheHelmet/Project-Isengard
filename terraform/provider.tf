c2e7879cd74eacd8460e7bdd61568c22c7992045
Helmet <ncarriage@gmail.com>
2024-04-27 14:02:53 +1000

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+  backend "s3" {
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+provider "http" {}
