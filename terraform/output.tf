792b805a7c536f179dec047d07c2afaf4a3e92ea
Helmet <ncarriage@gmail.com>
2024-04-27 15:40:23 +1000

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,4 @@
 output "server_ips" {
-  value = aws_instance.example_server.*.public_ip
+  value = aws_instance.linux_server.*.public_ip
   description = "The public IP addresses of the servers"
 }
