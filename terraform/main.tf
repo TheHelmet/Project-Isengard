8ae2ddee018fc8a33af3645ad419ca9123bcaae0
Helmet <ncarriage@gmail.com>
2024-04-27 17:28:21 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ user_data = <<-EOF
               #!/bin/bash
               # Install Docker
               sudo apt-get update
-              sudo apt-get install ca-certificates curl
+              sudo apt-get install -y ca-certificates curl
               sudo install -m 0755 -d /etc/apt/keyrings
               sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
               sudo chmod a+r /etc/apt/keyrings/docker.asc
