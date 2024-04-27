517bdecd5560f374e61da9c74a07205e75656dbe
Helmet <ncarriage@gmail.com>
2024-04-27 15:06:01 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,52 @@ resource "aws_instance" "linux_server" {
   user_data_base64 = base64encode(templatefile("cloudinit/userdata.tmpl", {
   }))
 }
+
+
+resource "aws_instance" "linux_server2" {
+
+  count                       = 0
+  ami                         = "ami-08e168f4c7bff7921"
+  instance_type               = "t2.medium"
+  key_name                    = var.key_pair
+  associate_public_ip_address = false
+  subnet_id                   = aws_subnet.subnet.id
+  vpc_security_group_ids      = ["${aws_security_group.security_group.id}"]
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = "15"
+  }
+
+  tags = {
+    "Name"    = "Isengard-2"
+    "project" = var.project
+  }
+
+  user_data_base64 = base64encode(templatefile("cloudinit/userdata.tmpl", {
+  }))
+}
+
+resource "aws_instance" "linux_server3" {
+
+  count                       = 0
+  ami                         = "ami-08e168f4c7bff7921"
+  instance_type               = "t2.medium"
+  key_name                    = var.key_pair
+  associate_public_ip_address = false
+  subnet_id                   = aws_subnet.subnet.id
+  vpc_security_group_ids      = ["${aws_security_group.security_group.id}"]
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = "15"
+  }
+
+  tags = {
+    "Name"    = "Isengard-3"
+    "project" = var.project
+  }
+
+  user_data_base64 = base64encode(templatefile("cloudinit/userdata.tmpl", {
+  }))
+}
