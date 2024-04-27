2d27b92cd214e9a6b00dd37d7aa45ee4ac2652f2
Helmet <ncarriage@gmail.com>
2024-04-27 17:30:54 +1000

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,10 @@
-output "server_ips" {
+output "main_server_ips" {
   value = { for i, inst in aws_instance.linux_server : inst.tags.Name => inst.public_ip }
+  description = "The public IP addresses of the servers"
+}
+
+
+output "web_server_ips" {
+  value = { for i, inst in aws_instance.nginx_server : inst.tags.Name => inst.public_ip }
   description = "The public IP addresses of the servers"
 }
