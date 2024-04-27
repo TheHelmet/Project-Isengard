e713ba17be286f82e426b5124231ca2a7cec3d6b
Helmet <ncarriage@gmail.com>
2024-04-27 15:02:39 +1000

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-nc"  # Replace with your bucket name
-    key    = state/isengard.tfstate"    # Path to the state file in the bucket
+    key    = "state/isengard.tfstate"    # Path to the state file in the bucket
     region = "ap-southeast-2"                  # Bucket region
     # Optionally, you can specify other settings like encryption, ACLs, etc.
   }
