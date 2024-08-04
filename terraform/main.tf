a6c4e1ef8ec423ab7ce07a4cf6eaa3e78a2f67ff
Helmet <ncarriage@gmail.com>
2024-08-04 16:31:57 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "linux_server" {
 
 resource "aws_instance" "nginx_server" {
 
-  count                       = 13
+  count                       = 0
   ami                         = "ami-08e168f4c7bff7921"
   instance_type               = "t2.micro"
   key_name                    = var.key_pair
